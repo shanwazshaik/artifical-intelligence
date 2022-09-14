92ce782d922c6a1f55a6308c34273453d4803c9b
shanwazshaik <113328105+shanwazshaik@users.noreply.github.com>
2022-09-14 23:50:57 +0530

    Add files via upload

---
+++ back chain.py
@@ -0,0 +1,8 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+colour(cherry, red).
+colour(banana, yellow).
+colour(apple, red).
+colour(apple, green).
+colour(orange, orange).
+colour(X, unknown).
