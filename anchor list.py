e294161522becfc3bb75dbf7ad029cf0baa44a3c
shanwazshaik <113328105+shanwazshaik@users.noreply.github.com>
2022-09-14 23:45:02 +0530

    Add files via upload

---
+++ anchor list.py
@@ -0,0 +1,7 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+<ul>
+<li><a href="#manually-create-anchor-links-wordpress">How to Manually Create Anchor Links in WordPress</a></li>
+<li><a href="#anchor-links-wordpress-plugin">How to Create Anchor Links in WordPress with a Plugin</a></li>
+<li><a href="#anchor-links-wordpress-gutenberg">How to Create Anchor Links in WordPress with Gutenberg</a></li>
+</ul>
